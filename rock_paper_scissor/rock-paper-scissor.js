482e67e3f303eba5bf3897e0befba8822b38d437
Vysakh Venugopalan <vysakhvenugopalan@gmail.com>
2023-11-04 19:36:28 +0530

    rock-paper-scissor game

---
+++ rock_paper_scissor/rock-paper-scissor.js
@@ -0,0 +1,38 @@
+let user=document.getElementById("user")
+let computer=document.getElementById("computer")
+let choice = ["rock","paper","scissor"]
+let result = document.createElement("h1")
+document.body.appendChild(result)
+let gg = document.querySelectorAll("h1")
+console.log(gg[1]+"hi")
+
+function getComputerChoice(e){
+    let computer_choice=choice[Math.floor(Math.random() * 3)]
+    let user_choice=e.target.id
+    user.innerHTML = user_choice
+    computer.innerHTML = computer_choice
+    switch(user_choice+computer_choice){
+        case "rockscissor":
+        case "scissorpaper":
+        case "paperrock": 
+                    result.innerHTML="You won. Well Done"
+                    break;
+        
+        case "scissorrock":
+        case "paperscissor":
+        case "rockpaper": 
+                result.innerHTML="You lost. Try Again"
+                break;
+
+        default :  result.innerHTML = "It's a draw"
+    }
+}
+
+
+choice.forEach(choice => {
+    let button=document.createElement('button')
+    button.id=choice
+    button.innerHTML=choice
+    addEventListener("click",getComputerChoice)
+    document.body.appendChild(button)
+});
